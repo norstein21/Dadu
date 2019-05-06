3eec02e99d8040e6c301d570674568a1593a2a5f
norstein21 <norstein4me@hotmail.co.id>
2019-05-07 05:52:39 +0700

    Initial Commit

---
+++ Dadu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dadu
+//
+//  Created by norstein21 on 07/05/19.
+//  Copyright © 2019 fntstic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
